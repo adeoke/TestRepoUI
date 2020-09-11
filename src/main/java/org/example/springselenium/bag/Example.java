88f1ac1eec30f03c9b41f96b404639c24fd6771d
adeoke <adesmail100@gmail.com>
2020-09-11 23:55:44 +0100

    reading from the application.properties example

--- src/main/java/org/example/springselenium/bag/Example.java
+++ src/main/java/org/example/springselenium/bag/Example.java
@@ -1,10 +1,22 @@
 package org.example.springselenium.bag;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+@PropertySource("application.properties")
 @Component
 public class Example {
+    private String urlPath;
+
+    @Autowired
+    public Example(@Value("${url.path}") String urlPath) {
+        this.urlPath = urlPath;
+    }
+    
     public void load() {
         System.out.println("implementing the load method......");
+        System.out.println("URL PATH: " + urlPath);
     }
 }
