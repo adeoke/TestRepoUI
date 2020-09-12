a8c2f683b34954526532d1c5f369c764e96e6ef2
adeoke <adesmail100@gmail.com>
2020-09-12 13:34:52 +0100

    removed unnecessary new line

--- src/test/java/org/example/springselenium/TestInjection.java
+++ src/test/java/org/example/springselenium/TestInjection.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
 public class TestInjection {
     @ExtendWith(SpringExtension.class)
     @ContextConfiguration(classes = Config.class)
